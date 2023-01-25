5854d62689ed360bce6cd0a027ad29b0c7786cf7
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2023-01-25 11:52:12 +0000

    first commit

---
+++ FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Model-View-Controller.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+/*:
+ [Previous](@previous)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Next](@next)
+ 
+ # Model-view-controller (MVC)
+ - - - - - - - - - -
+ ![MVC Diagram](MVC_Diagram.png)
+ 
+ The model-view-controller (MVC) pattern separates objects into three types: models, views and controllers.
+ 
+ **Models** hold onto application data. They are usually structs or simple classes.
+ 
+ **Views** display visual elements and controls on screen. They are usually subclasses of `UIView`.
+ 
+ **Controllers** coordinate between models and views. They are usually subclasses of `UIViewController`.
+ 
+ ## Code Example
+ */
+import UIKit
+
+//Mark: - Model
+public struct Address {
+    public var street: String
+    public var city: String
+    public var state: String
+    public var zipCode: String
+}
+
+// Mark: - View
+public final class AddressView: UIView {
+    @IBOutlet public var streetTextField: UITextField!
+    @IBOutlet public var cityTextField: UITextField!
+    @IBOutlet public var stateTextField: UITextField!
+    @IBOutlet public var zipCodeTextField: UITextField!
+    
+}
+
+// Mark: - Controller
+public final class AddressViewController: UIViewController {
+    //Mark: - Properties
+    public var address: Address? {
+        didSet {
+            updateViewFromAddress()
+        }
+    }
+    public var addressView: AddressView! {
+        guard isViewLoaded else {return nil}
+        return (view as! AddressView)
+    }
+    
+    //Mark: - View Lifecycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViewFromAddress()
+    }
+    
+    private func updateViewFromAddress() {
+        guard let addressView = addressView,
+              let address = address else {return}
+        addressView.streetTextField.text = address.street
+        addressView.cityTextField.text = address.city
+        addressView.stateTextField.text = address.state
+        addressView.zipCodeTextField.text = address.zipCode
+    }
+}
